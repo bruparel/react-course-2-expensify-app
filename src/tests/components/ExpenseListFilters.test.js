71693258c59e33355e027f890baf4890185de9fe
Bharat Ruparel <Bharat@wdvgco.com>
2017-11-11 13:44:25 -0600

    updated the webpack configuration

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -73,5 +73,5 @@ test('should handle date changes', () => {
 test('should handle date focus changes', () => {
     const calendarFocused = 'endDate'
     wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused)
-    expect(wrapper.state('calendarfocused')).toBe(calendarFocused)
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused)
 })
